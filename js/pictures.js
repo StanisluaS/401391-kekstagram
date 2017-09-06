8e412135894ee192dc178683e9ae39d08a72b099
StanisluaS <StanislauSkopenko@gmail.com>
2017-09-07 01:47:08 +0300

    Сделал модуль для работы с сервером

--- js/pictures.js
+++ js/pictures.js
@@ -3,23 +3,46 @@
 
 (function () {
 
+  window.uploadSelectImage = document.querySelector('#upload-select-image');
   window.similarListElement = document.querySelector('.pictures');
+  window.uploadOverlay = document.querySelector('.upload-overlay');
+  window.uploadImage = window.uploadSelectImage.querySelector('.upload-image');
   var similarFotoTemplate = document.querySelector('#picture-template').content;
 
-  printFoto(window.arrayFotos);
+  window.backend.load(printFoto, errorHandler);
 
-  function getFotos(foto) {
+  function errorHandler(errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  }
+
+  window.uploadSelectImage.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.backend.save(new FormData(window.uploadSelectImage), function () {
+      window.uploadOverlay.classList.add('hidden');
+      window.uploadImage.classList.remove('hidden');
+    }, errorHandler);
+  });
+
+  function getFotos(picturesData) {
     var fotoElement = similarFotoTemplate.cloneNode(true);
-    fotoElement.querySelector('img').setAttribute('src', foto.url);
-    fotoElement.querySelector('.picture-likes').textContent = foto.likes;
-    fotoElement.querySelector('.picture-comments').textContent = foto.comments.length;
+    fotoElement.querySelector('img').setAttribute('src', picturesData.url);
+    fotoElement.querySelector('.picture-likes').textContent = picturesData.likes;
+    fotoElement.querySelector('.picture-comments').textContent = picturesData.comments.length;
     return fotoElement;
   }
 
-  function printFoto(fotos) {
+  function printFoto(picturesData) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < fotos.length; i++) {
-      fragment.appendChild(getFotos(fotos[i]));
+    for (var i = 0; i < 25; i++) {
+      fragment.appendChild(getFotos(picturesData[i]));
     }
     window.similarListElement.appendChild(fragment);
   }
