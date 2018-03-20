a0b9ff8c49a3216780ea6401733baae123f10d2a
StanisluaS <StanislauSkopenko@gmail.com>
2018-03-20 22:56:52 +0300

    z

--- js/gallery.js
+++ js/gallery.js
@@ -48,27 +48,41 @@
 
   function openPopup(evt) {
     var target = evt.target;
-    if (target.classList.contains('pictures')) {
+    if (target === evt.currentTarget) {
       return;
+    }
+    if (target.classList.contains('picture')) {
+      openPopupWindow(target);
     } else {
       while (target.parentNode !== evt.currentTarget) {
         target = target.parentNode;
         if (target.classList.contains('picture')) {
-          window.preview.printFotoInGallery(target);
-          window.galleryOverlay.classList.remove('hidden');
-          galleryOverlayClose.addEventListener('click', closePopup);
-          document.addEventListener('keydown', onEscPress);
-          window.similarListElement.removeEventListener('click', openPopup);
-          window.similarListElement.removeEventListener('keydown', onEnterPress);
-          galleryOverlayClose.addEventListener('keydown', onEnterPress);
-          return;
+          openPopupWindow(target);
         }
       }
     }
   }
 
+  function openPopupWindow(el) {
+    window.preview.printFotoInGallery(el);
+    window.galleryOverlay.classList.remove('hidden');
+    window.pagesNext.addEventListener('click', window.pictures.leafImge);
+    window.pagesPrev.addEventListener('click', window.pictures.leafImge);
+    galleryOverlayClose.addEventListener('click', closePopup);
+    document.addEventListener('keydown', onEscPress);
+    window.similarListElement.removeEventListener('click', openPopup);
+    window.similarListElement.removeEventListener('keydown', onEnterPress);
+    galleryOverlayClose.addEventListener('keydown', onEnterPress);
+    return;
+  }
+
   function closePopup() {
     window.galleryOverlay.classList.add('hidden');
+    // window.index = 0;
+    // window.flag = true;
+    window.pictures.setZero();
+    window.pagesNext.removeEventListener('click', window.pictures.leafImge);
+    window.pagesPrev.removeEventListener('click', window.pictures.leafImge);
     galleryOverlayClose.removeEventListener('click', closePopup);
     document.removeEventListener('keydown', onEscPress);
     window.similarListElement.addEventListener('click', openPopup);
@@ -78,10 +92,11 @@
 
   function onEnterPress(evt) {
     if (evt.keyCode === ENTER_KEYCODE && evt.currentTarget === window.similarListElement) {
-      openPopup();
+      openPopup(evt);
     }
     if (evt.keyCode === ENTER_KEYCODE && evt.target === galleryOverlayClose) {
       closePopup();
+      window.pictures.setZero();
     }
     if (evt.keyCode === ENTER_KEYCODE && evt.target === uploadFormCancel) {
       closeOverlay();
@@ -94,6 +109,7 @@
       closePopup();
       closeOverlay();
       removeEffect();
+      window.pictures.setZero();
     }
   }
 
